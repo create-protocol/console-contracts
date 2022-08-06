ba42d5fe7e1e6bd5a01764a5542e080bb1da7d3f
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2022-08-06 16:13:05 +0530

    feat: add mockNFT
    Signed-off-by: 0xsmrpn <samarpan_d@pp.iitr.ac.in>

---
+++ contracts/MockNFT.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT License
+pragma solidity ^0.8.15;
+
+import "./ERC721Creator.sol";
+
+contract MockNFT is ERC721Creator {
+    constructor() ERC721Creator("name", "symbol", msg.sender) {}
+}
